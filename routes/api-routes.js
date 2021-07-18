405fbc7d09791a111ac86e820441263b086d9a16
Maggie Ward <mlward639@gmail.com>
2021-07-18 14:09:26 -0400

    simplified api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,6 @@
 // Get dependencies
 const path = require("path");
 const {v4 : uuidv4} = require('uuid');
-//const notes = require('../db/db.json');
 const fs = require('fs');
 const express = require('express');
 const app = express();
@@ -11,11 +10,13 @@ const app = express();
 //app.use(express.static('db'));
 
 module.exports = function(app){
+    // access saved notes from db.json file
+    let savedNotes = JSON.parse(fs.readFileSync('db/db.json'));
+
     // set up api/notes get route
     app.get('/api/notes', (req, res) => {
-        let notes = JSON.parse(fs.readFileSync('db/db.json'));
-        console.log(notes);
-        res.json(notes)        
+        console.log(savedNotes);
+        res.json(savedNotes)        
     });
 
     // set up api/notes post route
@@ -24,26 +25,15 @@ module.exports = function(app){
         newNote.id = uuidv4(); 
         console.log('new note', newNote);
         //access db.json array, push new note to that, then write new updated array back to db.json
-        let savedNotes = JSON.parse(fs.readFileSync('db/db.json'));
-        //console.log("Anything");
-        //console.log('before', savedNotes);
         savedNotes.push(newNote);
-        //console.log('after', savedNotes);
         fs.writeFileSync('db/db.json', JSON.stringify(savedNotes));
         res.json(newNote);
     });
     
     // set up api/notes delete route based on specific id
     app.delete('/api/notes/:id', (req, res) => {
-        let savedNotes = JSON.parse(fs.readFileSync('db/db.json'));
         const { id } = req.params.id;
         savedNotes.splice(id, 1);
         fs.writeFileSync('db/db.json', JSON.stringify(savedNotes));
         });
     }
-/*
-Maybe separate the fs.readFileSync and fs.writeFileSync into their own async functions
-2:20
-Then use await when you call those functions
-2:21
-Because, I feel like heroku is having a sync problem */
