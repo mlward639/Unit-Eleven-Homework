ade45872a4639f794d3aaa8578cf8f750bccd677
Maggie Ward <mlward639@gmail.com>
2021-07-18 01:51:43 -0400

    test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,11 +16,11 @@ module.exports = function(app){
         newNote.id = uuidv4(); 
         console.log('new note', newNote);
         //access db.json array, push new note to that, then write new updated array back to db.json
-        res.json(newNote);
-        console.log("Anything")
         let savedNotes = JSON.parse(fs.readFileSync('./db/db.json'));
         savedNotes.push(newNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+        res.json(newNote);
+        console.log("Anything");
     });
 }
 /* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you). */
